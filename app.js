09df5f824d406e0dfc0f3a33435a415b9034b4a0
nagulan <nagulans6524@gmail.com>
2026-02-09 06:36:51 +0530

    Fix: MongoStore import for connect-mongo v6

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const MongoStore = require('connect-mongo');
+const MongoStore = require('connect-mongo').default || require('connect-mongo');
 
 app.use(
   session({
