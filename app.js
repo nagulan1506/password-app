8061ddcdbd831ec1fdb1be0ca74046128031b483
nagulan <nagulans6524@gmail.com>
2026-02-09 06:15:20 +0530

    Fix deployment error: update wildcard route and build script

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'client/dist')));
 
 // Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
 });
 
